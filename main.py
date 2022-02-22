59b4ed9f7604a958a1329a617ac3787614fcae45
Ola-Daniel <doola-olorun@student.oauife.edu.ng>
2022-02-23 05:22:02 +0800

    Added more character, Script now generates 12 character passwords

--- main.py
+++ main.py
@@ -11,10 +11,24 @@ def shuffle(string):
 #    Main program
 uppercaseLetter1 = chr(random.randint(65, 90))   # Generate a random Uppercase letter (based on ASCII code)
 uppercaseLetter2 = chr(random.randint(65, 90))
+uppercaseLetter3 = chr(random.randint(65, 90))
+lowercaseLetter1 = chr(random.randint(97, 122))
+lowercaseLetter2 = chr(random.randint(97, 122))
+lowercaseLetter3 = chr(random.randint(97, 122))
+digit1 = chr(random.randint(48, 57))
+digit2 = chr(random.randint(48, 57))
+digit3 = chr(random.randint(48, 57))
+digit4 = chr(random.randint(48, 57))
+punch1 = chr(46)
+punch2 = chr(33)
+punch3 = chr(46)
+punch4 = chr(33)
+
 # Place-holder to generate more characters here
 
 # Generate password using all the characters, in random order
-password = uppercaseLetter1 + uppercaseLetter2    # + ......
+password = uppercaseLetter1 + uppercaseLetter2 + uppercaseLetter3 + lowercaseLetter1 + \
+           lowercaseLetter2 + lowercaseLetter3 + digit1 + digit2 + punch1 + punch2 + punch3 + punch4
 password = shuffle(password)
 
 print(password)
